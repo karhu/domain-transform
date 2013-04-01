ea0a6605770a11677726fdcf6b3528e1208dbd3a
Pascal Spörri <pascal@spoerri.io>
2013-04-01 18:32:31 +0200

    First version of the RF Filter

--- src/RF.h
+++ src/RF.h
@@ -13,12 +13,13 @@ void recursiveFilter(Mat2<float3> F, Mat2<float> dHdx, float sigma)
 
     Mat2<float> V(W, H);
 
-    for (int i=0; i<W; i++)
+    for (uint i=0; i<H; i++)
     {
-        for (int j=0; j<H; j++)
+        for (uint j=0; j<W; j++)
         {
             uint idx = i*W + j;
-            V.data[idx] = pow(a, dHdx.data[idx]);
+            float tmp = dHdx.data[idx];
+            V.data[idx] = pow(a, tmp);
         }
     }
 //    % Left -> Right filter.
@@ -27,15 +28,17 @@ void recursiveFilter(Mat2<float3> F, Mat2<float> dHdx, float sigma)
 //            F(:,i,c) = F(:,i,c) + V(:,i) .* ( F(:,i - 1,c) - F(:,i,c) );
 //        end
 //    end
-    for (int i=1; i<W; i++) // index starts from 1:w
+    for (uint i=0; i<H; i++)
+
     {
-        for (int j=0; j<H; j++)
+        for (uint j=1; j<W; j++) // index starts from 1:w
+
         {
+
             uint idx = i*W + j;
-            uint idx1 = (i-1)*W+j;
-            F.data[idx].r += V.data[idx]*(F.data[idx1].r-F.data[idx].r);
-            F.data[idx].g += V.data[idx]*(F.data[idx1].g-F.data[idx].g);
-            F.data[idx].b += V.data[idx]*(F.data[idx1].b-F.data[idx].b);
+            F.data[idx].r += V.data[idx]*(F.data[idx-1].r-F.data[idx].r);
+            F.data[idx].g += V.data[idx]*(F.data[idx-1].g-F.data[idx].g);
+            F.data[idx].b += V.data[idx]*(F.data[idx-1].b-F.data[idx].b);
         }
     }
 
@@ -46,15 +49,17 @@ void recursiveFilter(Mat2<float3> F, Mat2<float> dHdx, float sigma)
 //        end
 //    end
 
-    for (int i=W-2; i>0; i--) // index starts from w-1:-1:1
+    for (uint i=0; i<H; i++)
+
     {
-        for (int j=0; j<H; j++)
+        for (int j=W-2; j>=0; j--) // index starts from 1:w
+
         {
+
             uint idx = i*W + j;
-            uint idx1 = (i+1)*W+j;
-            F.data[idx].r += V.data[idx1]*(F.data[idx1].r-F.data[idx].r);
-            F.data[idx].g += V.data[idx1]*(F.data[idx1].g-F.data[idx].g);
-            F.data[idx].b += V.data[idx1]*(F.data[idx1].b-F.data[idx].b);
+            F.data[idx].r += V.data[idx+1]*(F.data[idx+1].r-F.data[idx].r);
+            F.data[idx].g += V.data[idx+1]*(F.data[idx+1].g-F.data[idx].g);
+            F.data[idx].b += V.data[idx+1]*(F.data[idx+1].b-F.data[idx].b);
         }
     }
 
@@ -62,14 +67,14 @@ void recursiveFilter(Mat2<float3> F, Mat2<float> dHdx, float sigma)
 }
 
 
-void filter(Mat2<float3> img, float sigma_s, float sigma_r, uint nIterations)
+void filter(Mat2<float3> F, float sigma_s, float sigma_r, uint nIterations)
 {
     //Estimate horizontal and vertical partial derivatives using finite differences.
-    Mat2<float3> dIcdx = diffX(img);
-    Mat2<float3> dIcdy = diffY(img);
+    Mat2<float3> dIcdx = diffX(F);
+    Mat2<float3> dIcdy = diffY(F);
 
-    const uint W = img.width;
-    const uint H = img.height;
+    const uint W = F.width;
+    const uint H = F.height;
 
     // Compute the l1-norm distance of neighbor pixels.
     Mat2<float> dIdx(W,H);
@@ -117,15 +122,13 @@ void filter(Mat2<float3> img, float sigma_s, float sigma_r, uint nIterations)
     // Perform the filtering
     uint n = nIterations;
 
-    Mat2<float3> &F = img;
-
     float sigma_H = sigma_s;
     for (uint i=0; i<nIterations; i++)
     {
-        float sigma_H_i = sigma_H*sqrt(3)*pow(2.0, n-(i+1)) / sqrt(pow(4,n)-1);
-        F = recursiveFilter(F, dHdx, sigma_H_i);
+        float sigma_H_i = sigma_H*sqrt(3)*powf(2.0, n-(i+1)) / sqrt(powf(4,n)-1);
+        recursiveFilter(F, dHdx, sigma_H_i);
         transpose(F);
-        F = recursiveFilter(F, dVdy, sigma_H_i);
+        recursiveFilter(F, dVdy, sigma_H_i);
         transpose(F);
     }
     // ...
