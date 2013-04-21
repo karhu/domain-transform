c3b941d0e964f4df140e027912b34f5b6e1868db
Pascal Spörri <pascal@spoerri.io>
2013-04-21 16:41:08 +0200

    Initial version of the validation tool

---
+++ validation_tool/main.py
@@ -0,0 +1,41 @@
+# Test Tool for Domain Transform Project
+# ======================================
+#
+#
+#
+# Needs PIL (execute pip install PIL as root)
+import tester
+import os
+default_image_dir = "../testimages"
+
+test_arguments = {
+    'nc': [
+        {'n': 1, 's': 40.0, 'r': 0.5},
+        {'n': 2, 's': 40.0, 'r': 0.5},
+        {'n': 3, 's': 40.0, 'r': 0.5},
+        {'n': 4, 's': 40.0, 'r': 0.5}
+    ],
+    'rf': [
+        {'n': 1, 's': 40.0, 'r': 0.5},
+        {'n': 2, 's': 40.0, 'r': 0.5},
+        {'n': 3, 's': 40.0, 'r': 0.5},
+        {'n': 4, 's': 40.0, 'r': 0.5}
+    ]
+}
+
+reference_argument = 'ref'
+
+test_binary = "../DomainTransform-build-GCC47-Default_build_Release/DomainTransform"
+reference_binary = test_binary
+
+
+def main():
+    image_dir = default_image_dir
+    files = [f for f in os.listdir(image_dir) if os.path.isfile(os.path.join(image_dir, f)) and f.endswith(".png")]
+    for method, arguments in test_arguments.iteritems():
+        for arg in arguments:
+            for f in files:
+                tester.run_test(test_binary, reference_binary, method, arg, f)
+
+if __name__ == "__main__":
+    main()
---
+++ validation_tool/tester.py
@@ -0,0 +1,63 @@
+import subprocess
+import os
+import math
+from PIL import Image
+
+REQUIRED_ACCURACY = 1e-3
+class ImageSizeDoesntMatchException(Exception):
+    pass
+class ImagesDontMatchException(Exception):
+    pass
+
+def validate(ref_filename, test_filename):
+    ref_im = Image.open(ref_filename).load()
+    test_im = Image.open(test_filename).load()
+
+    ref_size = ref_im.size()
+    test_size = test_im.size()
+    if (ref_size[0] != test_size[0] or ref_size[1] != test_size[1]):
+        raise ImageSizeDoesntMatchException("Image sizes don't match for {ref} and {test}. Expected {ref_size} but got {test_size}".format(
+            ref=ref_filename, test=test_filename, ref_size=ref_size, test_size=test_size))
+
+    errcounter = 0
+    for x in range(0,ref_size[0]):
+        for y in range(0, ref_size[1]):
+            rval = float(ref_im[x, y])
+            tval = float(test_im[x, y])
+            diff = math.fabs(rval-tval)
+            if (diff <= REQUIRED_ACCURACY):
+                print("({x}, {y}): Difference: {diff}".format(x=x, y=y, diff=diff))
+                errcounter += 1
+    if (errcounter > 0):
+        raise ImagesDontMatchException("Images don't match: Found {n} occurences".format(n=errcounter))
+
+def run_test(test_binary,  reference_binary, method, arguments, inputfile, test_dir="/tmp/validationtest",
+             reference_prefix="ref_", test_prefix="test_", run_reference_program=True):
+
+    file_prefix = os.path.basename(inputfile).replace(".png", "")
+    filename = file_prefix+"_"+str(method)+"__"+"___".join(arguments)
+    filename = filename.replace(".","_")+".png"
+    test_filename = test_dir+"/"+test_prefix+filename
+    ref_filename = test_dir+"/"+reference_prefix+filename
+
+    if (not os.path.exists(test_dir)):
+        os.makedirs(test_dir)
+
+    command_args = map(lambda l: "-"+str(l), arguments)+["-m",str(method)]+["-i",inputfile]
+    test_command_args = [test_binary]+command_args+["-o",test_filename]
+    ref_command_args = [reference_binary]+command_args+["-o",test_filename]
+
+    if (os.path.exists(test_filename)):
+        os.remove(test_filename)
+
+    print("Executing: ", str(test_command_args))
+    subprocess.check_call(test_command_args)
+
+    if (run_reference_program):
+        if (os.path.exists(ref_filename)):
+            os.remove(ref_filename)
+        print("Executing: ", str(ref_command_args))
+        subprocess.check_call(ref_command_args)
+
+    print("Validating")
+    validate(ref_filename, test_filename)
