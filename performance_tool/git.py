6afe0a4bb3b39b1cb0c343fbdc7aa7adedb636df
Pascal Spörri <pascal@spoerri.io>
2013-04-25 19:45:51 +0200

    Removed response var in git calls

--- performance_tool/git.py
+++ performance_tool/git.py
@@ -5,19 +5,19 @@ GIT_COMMAND = "git"
 
 def clone_repo(repo, directory=os.getcwd(), git_command=GIT_COMMAND):
     if (not os.path.exists(directory+"/.git")):
-        response=subprocess.check_output([git_command, "clone", repo, directory])
+        subprocess.check_output([git_command, "clone", repo, directory])
     else:
         print "Git Repository {dir} already exists. Pulling instead.".format(dir=directory)
         pull(directory, git_command)
 
 def checkout_branch(directory,branch, git_command=GIT_COMMAND):
     try:
-        response = subprocess.check_output([git_command, "checkout", "-b", branch, "remotes/origin/"+branch], cwd=directory)
+        subprocess.check_output([git_command, "checkout", "-b", branch, "remotes/origin/"+branch], cwd=directory)
     except:
-        response = subprocess.check_output([git_command, "checkout", branch], cwd=directory)
+        subprocess.check_output([git_command, "checkout", branch], cwd=directory)
 
 def pull(directory, git_command=GIT_COMMAND):
-    response = subprocess.check_output([git_command, "pull"], cwd=directory)
+    subprocess.check_output([git_command, "pull"], cwd=directory)
 
 def clean(directory, git_command=GIT_COMMAND):
-    response = subprocess.check_output([git_command, "clean", "-f"], cwd=directory)
+    subprocess.check_output([git_command, "clean", "-f"], cwd=directory)
