0fce8ed746cfa1d02eecaf07483a8c3a89d05594
Pascal Spörri <pascal@spoerri.io>
2013-05-01 21:05:04 +0200

    Better plotting support

--- plot_tool/main.py
+++ plot_tool/main.py
@@ -2,17 +2,19 @@ import numpy as np
 import re
 import matplotlib.pyplot as plt
 import matplotlib.cm as matplotlib_cm
+from PIL import Image
+
 
 DATA = {'local': {'results': [{'total_cycles': 109811000.0, 'benchmark_warmup': 0, 'description': 'NC (Normalized Convolution)', 'sigma_s': 20, 'sigma_r': 0.8, 'sum': [81938.5, 123906, 150657], 'iterations': 2, 'runtime_data': [{'total_cycles': 109789000.0, 'percentage': 100, 'cycles_call': 109789000.0, 'data': 'all', 'calls': 1}, {'total_cycles': 5712450.0, 'percentage': 5.20311, 'cycles_call': 5712450.0, 'data': 'diffX', 'calls': 1}, {'total_cycles': 5469500.0, 'percentage': 4.98182, 'cycles_call': 5469500.0, 'data': 'diffY', 'calls': 1}, {'total_cycles': 5415030.0, 'percentage': 4.93221, 'cycles_call': 5415030.0, 'data': 'domainTransform', 'calls': 1}, {'total_cycles': 1781570.0, 'percentage': 1.62272, 'cycles_call': 1781570.0, 'data': 'transpose_float', 'calls': 1}, {'total_cycles': 15055000.0, 'percentage': 13.7126, 'cycles_call': 3763750.0, 'data': 'transpose_rgb', 'calls': 4}, {'total_cycles': 1224380.0, 'percentage': 1.11521, 'cycles_call': 612190, 'data': 'cumsumX', 'calls': 2}, {'total_cycles': 39008200.0, 'percentage': 35.5301, 'cycles_call': 9752040.0, 'data': 'BoxFilterBounds', 'calls': 4}, {'total_cycles': 13188300.0, 'percentage': 12.0124, 'cycles_call': 3297070.0, 'data': 'computeRowSAT', 'calls': 4}, {'total_cycles': 18221100.0, 'percentage': 16.5964, 'cycles_call': 4555280.0, 'data': 'boxFilter', 'calls': 4}], 'benchmark_iterations': 1, 'input': './testimages/bhudda_640.png', 'cycles': 109811000.0, 'method': 'nc'}, {'total_cycles': 372372000.0, 'benchmark_warmup': 0, 'description': 'NC (Normalized Convolution)', 'sigma_s': 20, 'sigma_r': 0.8, 'sum': [283518, 410472, 486114], 'iterations': 2, 'runtime_data': [{'total_cycles': 372342000.0, 'percentage': 100, 'cycles_call': 372342000.0, 'data': 'all', 'calls': 1}, {'total_cycles': 16682400.0, 'percentage': 4.4804, 'cycles_call': 16682400.0, 'data': 'diffX', 'calls': 1}, {'total_cycles': 16223700.0, 'percentage': 4.3572, 'cycles_call': 16223700.0, 'data': 'diffY', 'calls': 1}, {'total_cycles': 14443900.0, 'percentage': 3.87921, 'cycles_call': 14443900.0, 'data': 'domainTransform', 'calls': 1}, {'total_cycles': 6674460.0, 'percentage': 1.79256, 'cycles_call': 6674460.0, 'data': 'transpose_float', 'calls': 1}, {'total_cycles': 55209200.0, 'percentage': 14.8276, 'cycles_call': 13802300.0, 'data': 'transpose_rgb', 'calls': 4}, {'total_cycles': 4293990.0, 'percentage': 1.15324, 'cycles_call': 2147000.0, 'data': 'cumsumX', 'calls': 2}, {'total_cycles': 128281000.0, 'percentage': 34.4525, 'cycles_call': 32070300.0, 'data': 'BoxFilterBounds', 'calls': 4}, {'total_cycles': 42299000.0, 'percentage': 11.3603, 'cycles_call': 10574700.0, 'data': 'computeRowSAT', 'calls': 4}, {'total_cycles': 59671100.0, 'percentage': 16.0259, 'cycles_call': 14917800.0, 'data': 'boxFilter', 'calls': 4}], 'benchmark_iterations': 1, 'input': './testimages/bhudda_720p.png', 'cycles': 372372000.0, 'method': 'nc'}, {'total_cycles': 285170000.0, 'benchmark_warmup': 0, 'description': 'NC (Normalized Convolution)', 'sigma_s': 20, 'sigma_r': 0.8, 'sum': [209560, 317067, 385548], 'iterations': 2, 'runtime_data': [{'total_cycles': 285142000.0, 'percentage': 100, 'cycles_call': 285142000.0, 'data': 'all', 'calls': 1}, {'total_cycles': 12637700.0, 'percentage': 4.43207, 'cycles_call': 12637700.0, 'data': 'diffX', 'calls': 1}, {'total_cycles': 12521400.0, 'percentage': 4.39127, 'cycles_call': 12521400.0, 'data': 'diffY', 'calls': 1}, {'total_cycles': 13464300.0, 'percentage': 4.72195, 'cycles_call': 13464300.0, 'data': 'domainTransform', 'calls': 1}, {'total_cycles': 5090610.0, 'percentage': 1.78529, 'cycles_call': 5090610.0, 'data': 'transpose_float', 'calls': 1}, {'total_cycles': 43234400.0, 'percentage': 15.1624, 'cycles_call': 10808600.0, 'data': 'transpose_rgb', 'calls': 4}, {'total_cycles': 3153400.0, 'percentage': 1.1059, 'cycles_call': 1576700.0, 'data': 'cumsumX', 'calls': 2}, {'total_cycles': 94707000.0, 'percentage': 33.2139, 'cycles_call': 23676700.0, 'data': 'BoxFilterBounds', 'calls': 4}, {'total_cycles': 33713100.0, 'percentage': 11.8232, 'cycles_call': 8428270.0, 'data': 'computeRowSAT', 'calls': 4}, {'total_cycles': 45589800.0, 'percentage': 15.9884, 'cycles_call': 11397400.0, 'data': 'boxFilter', 'calls': 4}], 'benchmark_iterations': 1, 'input': './testimages/bhudda_1024.png', 'cycles': 285170000.0, 'method': 'nc'}, {'total_cycles': 883744000.0, 'benchmark_warmup': 0, 'description': 'NC (Normalized Convolution)', 'sigma_s': 20, 'sigma_r': 0.8, 'sum': [637619, 923548, 1093750.0], 'iterations': 2, 'runtime_data': [{'total_cycles': 883721000.0, 'percentage': 100, 'cycles_call': 883721000.0, 'data': 'all', 'calls': 1}, {'total_cycles': 36959000.0, 'percentage': 4.18221, 'cycles_call': 36959000.0, 'data': 'diffX', 'calls': 1}, {'total_cycles': 35161900.0, 'percentage': 3.97885, 'cycles_call': 35161900.0, 'data': 'diffY', 'calls': 1}, {'total_cycles': 33495700.0, 'percentage': 3.7903, 'cycles_call': 33495700.0, 'data': 'domainTransform', 'calls': 1}, {'total_cycles': 16955800.0, 'percentage': 1.91869, 'cycles_call': 16955800.0, 'data': 'transpose_float', 'calls': 1}, {'total_cycles': 128424000.0, 'percentage': 14.5322, 'cycles_call': 32106100.0, 'data': 'transpose_rgb', 'calls': 4}, {'total_cycles': 10179600.0, 'percentage': 1.1519, 'cycles_call': 5089780.0, 'data': 'cumsumX', 'calls': 2}, {'total_cycles': 298454000.0, 'percentage': 33.7725, 'cycles_call': 74613600.0, 'data': 'BoxFilterBounds', 'calls': 4}, {'total_cycles': 105770000.0, 'percentage': 11.9688, 'cycles_call': 26442600.0, 'data': 'computeRowSAT', 'calls': 4}, {'total_cycles': 146403000.0, 'percentage': 16.5667, 'cycles_call': 36600900.0, 'data': 'boxFilter', 'calls': 4}], 'benchmark_iterations': 1, 'input': './testimages/bhudda_1080p.png', 'cycles': 883744000.0, 'method': 'nc'}, {'total_cycles': 1161160000.0, 'benchmark_warmup': 0, 'description': 'NC (Normalized Convolution)', 'sigma_s': 20, 'sigma_r': 0.8, 'sum': [835630, 1265350.0, 1538990.0], 'iterations': 2, 'runtime_data': [{'total_cycles': 1161140000.0, 'percentage': 100, 'cycles_call': 1161140000.0, 'data': 'all', 'calls': 1}, {'total_cycles': 52725600.0, 'percentage': 4.54085, 'cycles_call': 52725600.0, 'data': 'diffX', 'calls': 1}, {'total_cycles': 49942500.0, 'percentage': 4.30116, 'cycles_call': 49942500.0, 'data': 'diffY', 'calls': 1}, {'total_cycles': 43811500.0, 'percentage': 3.77314, 'cycles_call': 43811500.0, 'data': 'domainTransform', 'calls': 1}, {'total_cycles': 23459200.0, 'percentage': 2.02035, 'cycles_call': 23459200.0, 'data': 'transpose_float', 'calls': 1}, {'total_cycles': 182660000.0, 'percentage': 15.731, 'cycles_call': 45664900.0, 'data': 'transpose_rgb', 'calls': 4}, {'total_cycles': 13284100.0, 'percentage': 1.14406, 'cycles_call': 6642050.0, 'data': 'cumsumX', 'calls': 2}, {'total_cycles': 374084000.0, 'percentage': 32.217, 'cycles_call': 93521100.0, 'data': 'BoxFilterBounds', 'calls': 4}, {'total_cycles': 133015000.0, 'percentage': 11.4556, 'cycles_call': 33253800.0, 'data': 'computeRowSAT', 'calls': 4}, {'total_cycles': 188409000.0, 'percentage': 16.2262, 'cycles_call': 47102300.0, 'data': 'boxFilter', 'calls': 4}], 'benchmark_iterations': 1, 'input': './testimages/bhudda_2048.png', 'cycles': 1161160000.0, 'method': 'nc'}, {'total_cycles': 3861430000.0, 'benchmark_warmup': 0, 'description': 'NC (Normalized Convolution)', 'sigma_s': 20, 'sigma_r': 0.8, 'sum': [2966210.0, 3891280.0, 4364080.0], 'iterations': 2, 'runtime_data': [{'total_cycles': 3861410000.0, 'percentage': 100, 'cycles_call': 3861410000.0, 'data': 'all', 'calls': 1}, {'total_cycles': 162333000.0, 'percentage': 4.20399, 'cycles_call': 162333000.0, 'data': 'diffX', 'calls': 1}, {'total_cycles': 160276000.0, 'percentage': 4.1507, 'cycles_call': 160276000.0, 'data': 'diffY', 'calls': 1}, {'total_cycles': 153743000.0, 'percentage': 3.98153, 'cycles_call': 153743000.0, 'data': 'domainTransform', 'calls': 1}, {'total_cycles': 72499200.0, 'percentage': 1.87753, 'cycles_call': 72499200.0, 'data': 'transpose_float', 'calls': 1}, {'total_cycles': 619277000.0, 'percentage': 16.0376, 'cycles_call': 154819000.0, 'data': 'transpose_rgb', 'calls': 4}, {'total_cycles': 43295400.0, 'percentage': 1.12123, 'cycles_call': 21647700.0, 'data': 'cumsumX', 'calls': 2}, {'total_cycles': 1272250000.0, 'percentage': 32.9478, 'cycles_call': 318063000.0, 'data': 'BoxFilterBounds', 'calls': 4}, {'total_cycles': 429431000.0, 'percentage': 11.1211, 'cycles_call': 107358000.0, 'data': 'computeRowSAT', 'calls': 4}, {'total_cycles': 624277000.0, 'percentage': 16.1671, 'cycles_call': 156069000.0, 'data': 'boxFilter', 'calls': 4}], 'benchmark_iterations': 1, 'input': './testimages/bhudda_4k.png', 'cycles': 3861430000.0, 'method': 'nc'}], 'revision': 'local'}, 'computer_name': 'giauzar.local'}
 # plt.rc('text', usetex=True)
 # plt.rc('font', family='serif')
 
 
-IMAGE_REGEX = '.+\/.+_([\w]+)'
+IMAGE_REGEX = '.+\/([\w]+)'
 COMPILED_REGEX = re.compile(IMAGE_REGEX)
 
 # Will plot cycles per function
-def plot_branch_with_functions(name, data, sorted_img_list=["640", "1024",  "720p", "1080p", "2048", "4k"]):
+def plot_branch_with_functions(name, data, mode="cycles_pixel"):
     fig = plt.figure()
 
     results = data["results"]
@@ -21,12 +23,16 @@ def plot_branch_with_functions(name, data, sorted_img_list=["640", "1024",  "720
     # Functions
     tested_functions = []
     # Loop over all results and get results
+    image_size = []
     for resultset in results:
         method = resultset["description"]
         runtime_data = resultset["runtime_data"]
         input_file = COMPILED_REGEX.search(resultset["input"]).group(1)
         input_file = input_file.replace("_", " ")
 
+        im_handle = Image.open(resultset["input"])
+        im_size = im_handle.size[0]*im_handle.size[1]
+        image_size.append((input_file, im_size))
         # Store everything in a tuple for easy sorting
         rtuple = []
 
@@ -38,7 +44,11 @@ def plot_branch_with_functions(name, data, sorted_img_list=["640", "1024",  "720
             # Skip all functions containing all
             if fname == "all":
                 continue
-            data_dict[fname] = cycles
+            if (mode == "cycles_pixel"):
+                data_dict[fname] = cycles/im_size
+            else:
+                data_dict[fname] = cycles
+
             if not (fname in tested_functions):
                 tested_functions.append(fname)
             #rtuple += [(fname, perc)]
@@ -48,6 +58,8 @@ def plot_branch_with_functions(name, data, sorted_img_list=["640", "1024",  "720
     # Sort tested functions
     tested_functions = sorted(tested_functions)
 
+    image_size = sorted(image_size, key=lambda l: l[1])
+    sorted_img_list, sorted_img_size_list = zip(*image_size)
     # Now we have a dict with function names
     # and a dict with images names and corresponding cycles
     # we now map these values according to http://matplotlib.org/examples/pylab_examples/bar_stacked.html
@@ -79,15 +91,22 @@ def plot_branch_with_functions(name, data, sorted_img_list=["640", "1024",  "720
                 values[counter] = mapping[fn]
             counter += 1
         print values
-        pl = plt.bar(ind, values, width=bar_width, color=color, log=True)
+        pl = plt.bar(ind, values, width=bar_width, color=color)
         plt_bar_handles.append((pl, fn))
 
     plt_handles, plt_names = zip(*plt_bar_handles)
-    plt.xticks(map(lambda l: l+bar_width/2.0, ind), sorted_img_list)
+
+    megapixels = map(lambda l: l/1e6, sorted_img_size_list)
+    # Limit values to two decimal places
+    megapixels = map(lambda l: "%.2f" % l, megapixels)
+    plt.xticks(map(lambda l: l+bar_width/2.0, ind), megapixels)
 
     plt.legend(plt_handles, plt_names, loc="best")
-    plt.xlabel("Image Size")
-    plt.title("Cycles/Function (log)")
+    plt.xlabel("Image Size (Megapixels)")
+    if (mode == "cycles_pixel"):
+        plt.title("Cycles/Pixel")
+    else:
+        plt.title("Cycles")
     plt.grid(True)
 
 
