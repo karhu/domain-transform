f1c2915ed176f8bc3f0593e5fa4551e8f45fac72
Pascal Spörri <pascal@spoerri.io>
2013-05-02 23:25:54 +0200

    Added debug output to compile

--- performance_tool/compile.py
+++ performance_tool/compile.py
@@ -3,5 +3,6 @@ import subprocess
 QMAKE_BINARY = "qmake"
 MAKE_COMMAND = "make"
 def compile(qmake_file, cwd, qmake_binary=QMAKE_BINARY, make_command=MAKE_COMMAND):
+    print "Builiding "+qmake_file+" in "+cwd
     subprocess.check_call([qmake_binary, "-makefile", "-o", "Makefile", qmake_file], cwd=cwd)
     subprocess.check_call([make_command], cwd=cwd)
