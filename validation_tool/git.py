562d1ef5c067f301f38c93f944f119f4a0963021
Pascal Spörri <pascal@spoerri.io>
2013-05-04 20:30:31 +0200

    Added git support for reference images

---
+++ validation_tool/git.py
@@ -0,0 +1,49 @@
+import os
+import subprocess
+
+GIT_COMMAND = "git"
+
+def clone_repo(repo, directory=os.getcwd(), git_command=GIT_COMMAND):
+    print "Cloning {repo} into {directory}".format(repo=repo, directory=directory)
+    if (not os.path.exists(directory+"/.git")):
+        subprocess.check_output([git_command, "clone", repo, directory])
+    else:
+        print "Git Repository {dir} already exists. Pulling instead.".format(dir=directory)
+        pull(directory, git_command)
+
+def add_origin(remote, origin="origin", directory=os.getcwd(), git_command=GIT_COMMAND):
+    subprocess.check_output([git_command, "remote", "add", origin, remote], cwd=directory)
+
+def checkout_branch(directory,branch, git_command=GIT_COMMAND):
+    print "Checkout {branch} in {directory}".format(branch=branch, directory=directory)
+    subprocess.check_output([git_command, "fetch", "origin"], cwd=directory)
+    # try:
+    #     subprocess.check_output([git_command, "checkout", "-t", "origin/"+branch], cwd=directory)
+    # except:
+    subprocess.check_output([git_command, "checkout", "origin/"+branch], cwd=directory)
+
+def pull(directory, git_command=GIT_COMMAND):
+    print "Executing a pull in {directory}".format(directory=directory)
+    subprocess.check_output([git_command, "pull"], cwd=directory)
+
+def clean(directory, git_command=GIT_COMMAND):
+    print "Executing a clean in {directory}".format(directory=directory)
+    subprocess.check_output([git_command, "clean", "-f", "-n", "-d", "-x"], cwd=directory)
+
+def current_branch_name(directory, git_command=GIT_COMMAND):
+    o = subprocess.check_output([git_command, "rev-parse", "--abbrev-ref", "HEAD"])
+    return o.rstrip('\n')
+
+def current_revision(directory, git_command=GIT_COMMAND):
+    o = subprocess.check_output([git_command, "rev-parse", "HEAD"])
+    return o.rstrip('\n')
+
+def fetch_all(directory, git_command=GIT_COMMAND):
+    print "Fetching all from {directory}".format(directory=directory)
+
+    subprocess.check_call([git_command, "fetch", "-a"], cwd=directory)
+
+def replace_remote(directory, remote, origin="origin", git_command=GIT_COMMAND):
+    print "Replacing origin in {directory} with {origin}".format(origin=origin, directory=directory)
+    subprocess.check_call([git_command, "remote", "rm", origin], cwd=directory)
+    subprocess.check_call([git_command, "remote", "add", origin, remote], cwd=directory)
--- validation_tool/main.py
+++ validation_tool/main.py
@@ -6,6 +6,11 @@
 # Needs PIL (execute pip install PIL as root)
 import tester
 import os
+import git
+
+REFERENCE_IMAGES_GIT = "http://gitlab.rsp.li/domain-transform-project-reference-images.git"
+TEST_DIR = '/tmp/validationtest'
+
 default_image_dir = "../testimages"
 
 test_arguments = {
@@ -32,12 +37,13 @@ reference_binary = test_binary
 def main():
     image_dir = default_image_dir
     files = [image_dir+"/"+f for f in os.listdir(image_dir) if os.path.isfile(os.path.join(image_dir, f)) and f.endswith(".png")]
+    git.clone_repo(REFERENCE_IMAGES_GIT, TEST_DIR)
     for method, arguments in test_arguments.iteritems():
         for args in arguments:
             # Prepend a "-" for every key in the arguments dict and then flatten the list
             command_arguments = [i for tuple in args.items() for i in ("-"+str(tuple[0]),str(tuple[1]))]
             for f in files:
-                tester.run_test(test_binary, reference_binary, method, command_arguments, f, run_reference_program=True)
+                tester.run_test(test_binary, reference_binary, method, command_arguments, f, run_reference_program=False, test_dir=TEST_DIR)
 
 if __name__ == "__main__":
     main()
