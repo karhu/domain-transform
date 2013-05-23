f15c33ad9eb547f39fedfecc51bed8840cb958ed
Pascal Spörri <pascal@spoerri.io>
2013-05-23 14:46:39 +0200

    Added float array

--- src/NC.h
+++ src/NC.h
@@ -4,6 +4,11 @@
 #include "common.h"
 #include <cstring>
 
+#define MAX_DIV_ARRAY_VALUE 16
+const float DIV_ARRAY[(MAX_DIV_ARRAY_VALUE)+1] = {0.0,1.0,1.0/2.0,1.0/3.0,1.0/4.0,1.0/5.0,1.0/6.0,1.0/7.0,1.0/8.0,1.0/9.0,1.0/10.0,1.0/11.0,1.0/12.0,1.0/13.0,1.0/14.0,1.0/15.0,1.0/16.0};
+                              //,1.0/17.0,1.0/18.0,1.0/19.0,1.0/20.0,1.0/21.0,1.0/22.0,1.0/23.0,1.0/24.0,1.0/25.0,1.0/26.0,1.0/27.0,1.0/28.0,1.0/29.0,1.0/30.0,1.0/31.0,1.0/32.0,1.0/33.0,1.0/34.0,1.0/35.0,1.0/36.0,1.0/37.0,1.0/38.0,1.0/39.0,1.0/40.0,1.0/41.0,1.0/42.0,1.0/43.0,1.0/44.0,1.0/45.0,1.0/46.0,1.0/47.0,1.0/48.0,1.0/49.0,1.0/50.0,1.0/51.0,1.0/52.0,1.0/53.0,1.0/54.0,1.0/55.0,1.0/56.0,1.0/57.0,1.0/58.0,1.0/59.0,1.0/60.0,1.0/61.0,1.0/62.0,1.0/63.0,1.0/64.0,1.0/65.0,1.0/66.0,1.0/67.0,1.0/68.0,1.0/69.0,1.0/70.0,1.0/71.0,1.0/72.0,1.0/73.0,1.0/74.0,1.0/75.0,1.0/76.0,1.0/77.0,1.0/78.0,1.0/79.0,1.0/80.0,1.0/81.0,1.0/82.0,1.0/83.0,1.0/84.0,1.0/85.0,1.0/86.0,1.0/87.0,1.0/88.0,1.0/89.0,1.0/90.0,1.0/91.0,1.0/92.0,1.0/93.0,1.0/94.0,1.0/95.0,1.0/96.0,1.0/97.0,1.0/98.0,1.0/99.0,1.0/100.0};
+// generated with ",".join(map(lambda l: str(1.0)+"/"+str(float(l)), range(0,101)))
+
 namespace NC
 {
 
@@ -50,7 +55,13 @@ void TransformedDomainBoxFilter(Mat2<float3>& img,
             }
 
             int delta = posR - posL;
-            float invD = 1.0f / delta;
+            float invD;
+            if (delta <= (MAX_DIV_ARRAY_VALUE)) {
+                invD = DIV_ARRAY[delta];
+            } else
+            {
+                invD = 1.0f / delta;
+            }
 
             imgOut.data[idxT].r = sum.r * invD;
             imgOut.data[idxT].g = sum.g * invD;
@@ -134,7 +145,6 @@ void filter(Mat2<float3>& img, float sigma_s, float sigma_r, uint nIterations)
     {
         // Compute the sigma value for this iteration
         float sigmaHi = sigma_s * sqrt(3) * powf(2.0,(nIterations - (i+1))) / sqrt(powf(4,nIterations)-1);
-
         // Compute the radius of the box filter with the desired variance.
         float boxR = sqrt(3) * sigmaHi;
 
